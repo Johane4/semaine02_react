a200c6ff159442d2f5ea88646caae00431d8e99b
Johane4 <omiscar_johane@ymail.com>
2021-12-14 11:50:50 +0100

    ajout corrigé

---
+++ 3_hooks_contextAPI/exercice-context/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Container from './components/Container';
+
+ const App = () => {
+
+    return (
+        <div className="container">
+            <Container />
+        </div>
+    )
+}
+
+
+export default App;
---
+++ 3_hooks_contextAPI/exercice-context/src/reducers/NumbersProvider.js
@@ -0,0 +1,59 @@
+import { useEffect, useReducer } from 'react';
+import NumberContext from '../context/NumberContext';
+
+
+const initialState = {
+    numbers: [],
+    // number: ""
+}
+
+const reducer = (state, action) => {
+    switch (action.type) {
+
+        case 'INIT':
+            return {
+                ...state,
+                numbers: action.numbers
+            }
+        case 'SET_NUMBER':
+            return {
+                ...state,
+                number: action.number
+            }
+        case 'ADD_NUMBER':
+            const numbers = [...state.numbers]
+            numbers.push(Number( state.number ));
+
+            return {
+                ...state,
+                number: '',
+                numbers: numbers
+            }
+
+        default:
+            return state
+    }
+}
+
+const NumbersProvider = ({ children }) => {
+
+    const [state, dispatch] = useReducer(reducer, initialState);
+
+    useEffect(() => {
+        dispatch({
+            type: 'INIT', numbers: [
+                20,
+                43,
+                78
+            ]
+        })
+    }, [])
+
+    return (
+        <NumberContext.Provider value={[state, dispatch]}>
+            {children}
+        </NumberContext.Provider>
+    )
+}
+
+export default NumbersProvider;
